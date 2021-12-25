bcd750e8c5ce260c21db583b1f7606eb0cd1fee1
Ahmed Yasser <ayasser@softmills.com>
2021-12-25 11:35:11 +0200

    Added Changes

--- packages/stream_chat_flutter/lib/src/stream_chat_theme.dart
+++ packages/stream_chat_flutter/lib/src/stream_chat_theme.dart
@@ -280,7 +280,7 @@ class StreamChatThemeData {
             return StreamSvgIcon.loveReaction(
               color: highlighted
                   ? theme.colorTheme.accentPrimary
-                  : theme.primaryIconTheme.color!.withOpacity(0.5),
+                  : Colors.red.withOpacity(0.5),
               size: size,
             );
           },
